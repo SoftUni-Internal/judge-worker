e2aa55b6c65712172d301e2dada7c7a0075cef33
Georgi Georgiev <gogo4ds@gmail.com>
2018-12-20 16:52:31 +0200

    Ported OJS.Workers.Common to .NET Standard 2.0 closes https://github.com/SoftUni-Internal/interactive-issues/issues/159

--- OJS.Workers.Common/Helpers/Code.cs
+++ OJS.Workers.Common/Helpers/Code.cs
@@ -1,9 +1,6 @@
 ﻿namespace OJS.Workers.Common.Helpers
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
 
     public static class Code

aa96b1cb46f4e8c3452d32d0f711759180e4a1cf
Georgi Georgiev <gogo4ds@gmail.com>
2019-02-04 16:14:24 +0200

    Added ExecutionResultExtension for limiting output result length https://github.com/SoftUni-Internal/interactive-issues/issues/198

---
+++ OJS.Workers.ExecutionStrategies/Extensions/ExecutionResultExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace OJS.Workers.ExecutionStrategies.Extensions
+{
+    using OJS.Workers.Common.Extensions;
+    using OJS.Workers.ExecutionStrategies.Models;
+
+    public static class ExecutionResultExtensions
+    {
+        private const string ExceededOutputMaxLengthDefaultWarningMessageFormat =
+            "... [Output length exceeds the allowed limit of {0} characters]";
+
+        public static void LimitLength(this OutputResult result, int lengthLimit, string appendMessage = null)
+        {
+            if (result == null)
+            {
+                return;
+            }
+
+            if (result.Output.Length <= lengthLimit)
+            {
+                appendMessage = string.Empty;
+            }
+            else if (appendMessage == null)
+            {
+                appendMessage = string.Format(ExceededOutputMaxLengthDefaultWarningMessageFormat, lengthLimit);
+            }
+
+            result.Output = result.Output.MaxLength(lengthLimit) + appendMessage;
+        }
+    }
+}

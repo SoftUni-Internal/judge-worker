ea49be4947badffed43ebc0cdcda745e280a2ccc
Georgi Georgiev <gogo4ds@gmail.com>
2019-02-07 11:04:20 +0200

    CR fixes: used arrow syntax https://github.com/SoftUni-Internal/interactive-issues/issues/250

--- OJS.Workers.Common/Extensions/CollectionExtensions.cs
+++ OJS.Workers.Common/Extensions/CollectionExtensions.cs
@@ -5,11 +5,6 @@
     public static class CollectionExtensions
     {
         public static void AddRange<T>(this ICollection<T> destination, IEnumerable<T> source)
-        {
-            foreach (var item in source)
-            {
-                destination.Add(item);
-            }
-        }
+            => source.ForEach(destination.Add);
     }
 }

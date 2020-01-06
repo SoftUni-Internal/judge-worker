1778dd91cc15fa1fce80201b81aea0efda40b3b3
gogo4ds <gogo4ds@gmail.com>
2020-01-06 13:54:32 +0200

    Formatting
    https://github.com/SoftUni-Internal/suls-issues/issues/5482

--- OJS.Workers.Common/Exceptions/InvalidProcessExecutionOutput.cs
+++ OJS.Workers.Common/Exceptions/InvalidProcessExecutionOutput.cs
@@ -4,7 +4,8 @@
 
     public class InvalidProcessExecutionOutput : Exception
     {
-        private const string CustomMessage = "The process did not produce any valid output! " +
+        private const string CustomMessage =
+            "The process did not produce any valid output! " +
             "Please try again later or contact an administrator if the problem persists.";
 
         public InvalidProcessExecutionOutput()

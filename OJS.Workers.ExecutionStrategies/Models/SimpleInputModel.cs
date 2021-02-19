1e0404fe77270eaecc9d07ab0cc233ceb8a41414
Doncho Minkov <DonchoMinkov@gmail.com>
2021-02-19 13:30:30 +0200

    Removed wrong redefining of properties Progress on https://github.com/SoftUni-Internal/interactive-issues/issues/1914t

--- OJS.Workers.ExecutionStrategies/Models/SimpleInputModel.cs
+++ OJS.Workers.ExecutionStrategies/Models/SimpleInputModel.cs
@@ -1,14 +1,8 @@
 ﻿namespace OJS.Workers.ExecutionStrategies.Models
 {
-    using OJS.Workers.Common.Extensions;
-
     public class SimpleInputModel
         : BaseInputModel
     {
-        public byte[] TaskSkeleton { get; set; }
-
-        public string TaskSkeletonAsString => this.TaskSkeleton.Decompress();
-
         public string Input { get; set; }
     }
 }

87378cf14f7bc3adc5b8b010283043ec749181df
Doncho Minkov <donchominkov@gmail.com>
2021-06-01 16:51:38 +0300

    Fixed a bug when enabled or disabled strategies are empy

--- OJS.Workers.SubmissionProcessors/ExecutionTypeFilters/SubmissionFilteringServiceBase.cs
+++ OJS.Workers.SubmissionProcessors/ExecutionTypeFilters/SubmissionFilteringServiceBase.cs
@@ -1,5 +1,6 @@
 ﻿namespace OJS.Workers.SubmissionProcessors.ExecutionTypeFilters
 {
+    using System;
     using System.Collections.Generic;
     using OJS.Workers.Common;
     using OJS.Workers.Common.Models;
@@ -14,9 +15,17 @@
 
         public bool CanProcessSubmission(IOjsSubmission submission, ISubmissionWorker submissionWorker)
             => submission != null
-                && !this.DisabledExecutionStrategyTypes.Contains(submission.ExecutionStrategyType)
-                && this.EnabledExecutionStrategyTypes.Contains(submission.ExecutionStrategyType)
+                && !this.IsDisabledStrategy(submission)
+                && this.IsEnabledStrategy(submission)
                 && this.CanProcessSubmissionInternal(submission, submissionWorker);
+
+        private bool IsDisabledStrategy(IOjsSubmission submission)
+            => this.DisabledExecutionStrategyTypes.Count > 0
+                 && this.DisabledExecutionStrategyTypes.Contains(submission.ExecutionStrategyType);
+
+        private bool IsEnabledStrategy(IOjsSubmission submission)
+        => this.EnabledExecutionStrategyTypes.Count == 0
+                || this.EnabledExecutionStrategyTypes.Contains(submission.ExecutionStrategyType);
 
         protected virtual bool CanProcessSubmissionInternal(IOjsSubmission submission, ISubmissionWorker submissionWorker)
             => true;

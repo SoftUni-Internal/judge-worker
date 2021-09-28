30a6ba2c1ebe7ec7f997f904036dd8c452960109
shsimeonova <simona.simeonova@hotmail.com>
2021-09-28 17:02:34 +0300

    Fixed build error with parameters on separate row

--- OJS.Workers.ExecutionStrategies/CSharp/DotNetCoreUnitTestsExecutionStrategy.cs
+++ OJS.Workers.ExecutionStrategies/CSharp/DotNetCoreUnitTestsExecutionStrategy.cs
@@ -80,8 +80,7 @@
                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
             var compilerPath = this.GetCompilerPathFunc(executionContext.CompilerType);
-            var testedCodePath = FileHelpers.BuildPath(this.NUnitLiteConsoleAppDirectory,
-                UnitTestStrategiesHelper.TestedCodeFileNameWithExtension);
+            var testedCodePath = FileHelpers.BuildPath(this.NUnitLiteConsoleAppDirectory, UnitTestStrategiesHelper.TestedCodeFileNameWithExtension);
             var originalTestsPassed = -1;
 
             var tests = executionContext.Input.Tests.OrderBy(x => x.IsTrialTest).ThenBy(x => x.OrderBy).ToList();

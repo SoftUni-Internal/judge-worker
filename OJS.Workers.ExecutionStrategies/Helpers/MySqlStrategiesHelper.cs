bcf662a59bdb49da51b8a1f6fd660ac8f435ac57
gogo4ds <gogo4ds@gmail.com>
2022-02-08 19:40:57 +0200

    Added method for optimizing insert statements in mysql query

---
+++ OJS.Workers.ExecutionStrategies/Helpers/MySqlStrategiesHelper.cs
@@ -0,0 +1,64 @@
+﻿namespace OJS.Workers.ExecutionStrategies.Helpers
+{
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public static class MySqlStrategiesHelper
+    {
+        private const string InsertIntoTableRegexPattern = @"insert\s+into\s+([^(]+)\s+\([^(]+\)\s+values\s*";
+        private const string NewLineSplitRegexPattern = @"\r|\n|\r\n";
+
+        public static string TryOptimizeQuery(string query)
+        {
+            var newQuery = new StringBuilder();
+
+            var lines = Regex.Split(query, NewLineSplitRegexPattern);
+            var insertStatementRegex = new Regex(InsertIntoTableRegexPattern);
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var currLine = lines[i];
+                var prevLine = i > 0 ? lines[i - 1] : string.Empty;
+                var nextLine = i < lines.Length - 1 ? lines[i + 1] : string.Empty;
+
+                if (insertStatementRegex.IsMatch(currLine))
+                {
+                    currLine = FormatInsertStatement(currLine, prevLine, nextLine, insertStatementRegex);
+                }
+
+                newQuery.AppendLine(currLine);
+            }
+
+            return newQuery.ToString();
+        }
+
+        private static string FormatInsertStatement(
+            string currLine,
+            string prevLine,
+            string nextLine,
+            Regex insertStatementRegex)
+        {
+            var prevLineIsInsertStatement = insertStatementRegex.IsMatch(prevLine);
+            var nextLineIsInsertStatement = insertStatementRegex.IsMatch(nextLine);
+
+            if (prevLineIsInsertStatement || nextLineIsInsertStatement)
+            {
+                var currLineInsertTable = insertStatementRegex.Match(currLine).Groups[1].Value;
+                var prevLineInsertTable = insertStatementRegex.Match(prevLine).Groups[1].Value;
+                var nextLineInsertTable = insertStatementRegex.Match(nextLine).Groups[1].Value;
+
+                if (currLineInsertTable == prevLineInsertTable)
+                {
+                    currLine = insertStatementRegex.Replace(currLine, string.Empty);
+                }
+
+                if (currLineInsertTable == nextLineInsertTable)
+                {
+                    currLine = currLine.TrimEnd(';') + ',';
+                }
+            }
+
+            return currLine;
+        }
+    }
+}

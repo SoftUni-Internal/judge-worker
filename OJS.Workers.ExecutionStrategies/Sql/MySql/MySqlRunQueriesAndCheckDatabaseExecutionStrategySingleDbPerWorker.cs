eb62e6fe35a231ad524d970e35783f71d1c28053
Mihail Petrov <43758817+mishogv@users.noreply.github.com>
2022-09-21 17:11:58 +0300

    Added fix for single db strategy

--- OJS.Workers.ExecutionStrategies/Sql/MySql/MySqlRunQueriesAndCheckDatabaseExecutionStrategySingleDbPerWorker.cs
+++ OJS.Workers.ExecutionStrategies/Sql/MySql/MySqlRunQueriesAndCheckDatabaseExecutionStrategySingleDbPerWorker.cs
@@ -1,5 +1,6 @@
 ﻿#pragma warning disable SA1200
 using MySql.Data.MySqlClient;
+
 #pragma warning restore SA1200
 
 namespace OJS.Workers.ExecutionStrategies.Sql.MySql
@@ -89,6 +90,26 @@ namespace OJS.Workers.ExecutionStrategies.Sql.MySql
             {
                 result.IsCompiledSuccessfully = false;
                 result.CompilerComment = ex.Message;
+                using (var connection = this.GetOpenConnection(databaseName))
+                {
+                    var cleanUpAfterTest = $@"
+                        START TRANSACTION ;
+                        SET FOREIGN_KEY_CHECKS = 0;
+                        SET GROUP_CONCAT_MAX_LEN=32768;
+                        SET @tables = NULL;
+                        SELECT GROUP_CONCAT('`', table_name, '`') INTO @tables
+                        FROM information_schema.tables
+                        WHERE table_schema = '{databaseName}';
+                        SELECT IFNULL(@tables,'dummy') INTO @tables;
+                        SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
+                        PREPARE stmt FROM @tables;
+                        EXECUTE stmt;
+                        DEALLOCATE PREPARE stmt;
+                        SET FOREIGN_KEY_CHECKS = 1;
+                        COMMIT ;";
+
+                    this.ExecuteNonQuery(connection, cleanUpAfterTest);
+                }
             }
 
             result.StopWatchResult = st.Elapsed.ToString();

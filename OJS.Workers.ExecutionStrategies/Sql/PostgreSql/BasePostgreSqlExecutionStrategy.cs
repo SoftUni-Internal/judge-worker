f7a101bdddbe918cf211de92160e5f7e813e8ab6
mishogv <mihogv@gmail.com>
2023-03-16 12:23:48 +0200

    Fix datetime time format https://github.com/SoftUni-Internal/exam-systems-issues/issues/545

--- OJS.Workers.ExecutionStrategies/Sql/PostgreSql/BasePostgreSqlExecutionStrategy.cs
+++ OJS.Workers.ExecutionStrategies/Sql/PostgreSql/BasePostgreSqlExecutionStrategy.cs
@@ -10,7 +10,7 @@
 
     public abstract class BasePostgreSqlExecutionStrategy : BaseSqlExecutionStrategy
     {
-        private const string DateTimeFormat = "yyyy-MM-dd hh:mm:ss";
+        private const string DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
         private const string TimeSpanFormat = @"hh\:mm\:ss";
 
         private readonly string databaseNameForSubmissionProcessor;

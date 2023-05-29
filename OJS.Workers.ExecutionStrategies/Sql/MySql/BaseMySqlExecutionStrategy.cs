5f01c4cd9658cd17096913d1ed18a8056a101286
Georgi Georgiev <gogo4ds@gmail.com>
2023-05-29 19:03:04 +0300

    Added custom message on specific errorOC

--- OJS.Workers.ExecutionStrategies/Sql/MySql/BaseMySqlExecutionStrategy.cs
+++ OJS.Workers.ExecutionStrategies/Sql/MySql/BaseMySqlExecutionStrategy.cs
@@ -1,4 +1,7 @@
-﻿namespace OJS.Workers.ExecutionStrategies.Sql.MySql
+﻿using OJS.Workers.Common;
+using OJS.Workers.ExecutionStrategies.Models;
+
+namespace OJS.Workers.ExecutionStrategies.Sql.MySql
 {
     using System;
     using System.Data;
@@ -38,6 +41,29 @@
             this.sysDbConnectionString = sysDbConnectionString;
             this.restrictedUserId = restrictedUserId;
             this.restrictedUserPassword = restrictedUserPassword;
+        }
+
+        protected override IExecutionResult<TestResult> Execute(
+            IExecutionContext<TestsInputModel> executionContext, IExecutionResult<TestResult> result, Action<IDbConnection, TestContext> executionFlow)
+        {
+            base.Execute(executionContext, result, executionFlow);
+
+            // TODO: Fix concurrent execution of SQL queries.
+            // This is a temporary fix for the following error,
+            // but the strategy should be reworked to avoid this error in the first place.
+            // It happens rarely, but it still happens. Chances are it will not happen again on the next execution.
+            const string ConcurrencyExceptionMessage =
+                "The ReadAsync method cannot be called when another read operation is pending.";
+
+            if (!result.IsCompiledSuccessfully &&
+                (result.CompilerComment
+                    ?.Trim()
+                    .Equals(ConcurrencyExceptionMessage, StringComparison.InvariantCultureIgnoreCase) ?? false))
+            {
+                result.CompilerComment = "Please, re-submit your solution. If the problem persists, contact an administrator.";
+            }
+
+            return result;
         }
 
         public override IDbConnection GetOpenConnection(string databaseName)

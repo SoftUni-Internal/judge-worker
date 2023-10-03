cc8ad0ebe7921982d5820ee84495353fec9ef030
Petar Neshkov <petar.neshkov@softuni.bg>
2023-10-03 18:09:51 +0300

    Removed unnecessary strategies

--- OJS.Workers.SubmissionProcessors/Helpers/SubmissionProcessorHelper.cs
+++ OJS.Workers.SubmissionProcessors/Helpers/SubmissionProcessorHelper.cs
@@ -4,7 +4,6 @@
     using OJS.Workers.Common;
     using OJS.Workers.Common.Models;
     using OJS.Workers.ExecutionStrategies;
-    using OJS.Workers.ExecutionStrategies.Blockchain;
     using OJS.Workers.ExecutionStrategies.CPlusPlus;
     using OJS.Workers.ExecutionStrategies.CSharp;
     using OJS.Workers.ExecutionStrategies.CSharp.DotNetCore;
@@ -18,7 +17,6 @@
     using OJS.Workers.ExecutionStrategies.Ruby;
     using OJS.Workers.ExecutionStrategies.Sql.MySql;
     using OJS.Workers.ExecutionStrategies.Sql.PostgreSql;
-    using OJS.Workers.ExecutionStrategies.Sql.SqlServerLocalDb;
     using OJS.Workers.ExecutionStrategies.Sql.SqlServerSingleDatabase;
     using OJS.Workers.Executors.Implementations;
     using OJS.Workers.SubmissionProcessors.Models;

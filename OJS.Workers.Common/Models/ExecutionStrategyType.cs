d5c9c5762893e8faa77de509f99c32946cf99152
Simona Simeonova <simona.simeonova@softuni.bg>
2023-11-08 17:30:47 +0200

    Added support for both java 11 and java 17

--- OJS.Workers.Common/Models/ExecutionStrategyType.cs
+++ OJS.Workers.Common/Models/ExecutionStrategyType.cs
@@ -54,8 +54,10 @@
         PostgreSqlRunQueriesAndCheckDatabase = 59,
         PostgreSqlRunSkeletonRunQueriesAndCheckDatabase = 60,
         PythonDjangoOrmExecutionStrategy = 61,
-        Java17PreprocessCompileExecuteAndCheck = 62,
+        Java17ProjectTestsExecutionStrategy = 62,
         Java17ZipFileCompileExecuteAndCheck = 63,
-        Java17SpringAndHibernateProjectExecution = 64,
+        Java17UnitTestsExecutionStrategy = 64,
+        Java17SpringAndHibernateProjectExecution = 65,
+        Java17PreprocessCompileExecuteAndCheck = 66
     }
 }

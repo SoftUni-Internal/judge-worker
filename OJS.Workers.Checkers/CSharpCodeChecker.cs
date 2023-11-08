eb5503c6f7bfa712de0cbc81e49c8a079413c691
Petar Neshkov <petar.neshkov@softuni.bg>
2023-11-08 15:20:35 +0200

    Refactored csharpcode to use roslyn.

--- OJS.Workers.Checkers/CSharpCodeChecker.cs
+++ OJS.Workers.Checkers/CSharpCodeChecker.cs
@@ -1,10 +1,13 @@
-﻿namespace OJS.Workers.Checkers
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace OJS.Workers.Checkers
 {
     using System;
-    using System.CodeDom.Compiler;
     using System.Text;
-
-    using Microsoft.CSharp;
     using OJS.Workers.Checkers.CSharpCodeCheckers;
 
     public class CSharpCodeChecker
@@ -12,36 +15,47 @@
     {
         protected override Type CompileCheckerAssembly(string sourceCode)
         {
-            var codeProvider = new CSharpCodeProvider();
-            var compilerParameters = new CompilerParameters { GenerateInMemory = true, };
-            compilerParameters.ReferencedAssemblies.Add("System.dll");
-            compilerParameters.ReferencedAssemblies.Add("System.Core.dll");
-            compilerParameters.ReferencedAssemblies.Add("OJS.Workers.Common.dll");
-            var compilerResults = codeProvider.CompileAssemblyFromSource(compilerParameters, sourceCode);
+           var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+            var assemblyPath = Path.GetDirectoryName(typeof(object).Assembly.Location);
 
-            this.CheckForErrors(compilerResults);
-            return this.GetCustomCheckerType(compilerResults.CompiledAssembly);
-        }
-
-        private void CheckForErrors(CompilerResults compilerResults)
-        {
-            if (!compilerResults.Errors.HasErrors)
+            var references = new List<MetadataReference>
             {
-                return;
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Regex).Assembly.Location),
+                MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.Runtime.dll")),
+                MetadataReference.CreateFromFile("OJS.Workers.Common.dll"),
+
+            };
+
+            var compilation = CSharpCompilation.Create(
+                "CheckerAssembly",
+                new[] { syntaxTree },
+                references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using var ms = new MemoryStream();
+            var result = compilation.Emit(ms);
+
+            if (!result.Success)
+            {
+                var failures = result.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError ||
+                    diagnostic.Severity == DiagnosticSeverity.Error);
+
+                var errorsStringBuilder = new StringBuilder();
+                foreach (var diagnostic in failures)
+                {
+                    errorsStringBuilder.AppendLine($"{diagnostic.Id}: {diagnostic.GetMessage()}");
+                }
+
+                throw new Exception(
+                    $"Could not compile checker!{Environment.NewLine}Errors:{Environment.NewLine}{errorsStringBuilder}");
             }
 
-            var errorsStringBuilder = new StringBuilder();
-            foreach (CompilerError error in compilerResults.Errors)
-            {
-                errorsStringBuilder.AppendLine(error.ToString());
-            }
-
-            // TODO: Introduce class CompilerException and throw exception of this type
-            throw new Exception(
-                string.Format(
-                    "Could not compile checker!{0}Errors:{0}{1}",
-                    Environment.NewLine,
-                    errorsStringBuilder));
+            ms.Seek(0, SeekOrigin.Begin);
+            var assembly = Assembly.Load(ms.ToArray());
+            return this.GetCustomCheckerType(assembly);
         }
     }
 }

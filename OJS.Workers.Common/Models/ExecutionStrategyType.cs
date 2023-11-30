da078049588afbbb9b2a5868ee75a52c8f5201da
Mihail Tsatsarov <MihailTsatsarov92@gmail.com>
2023-11-30 14:55:47 +0200

    Add NodeJs v20 strategies

--- OJS.Workers.Common/Models/ExecutionStrategyType.cs
+++ OJS.Workers.Common/Models/ExecutionStrategyType.cs
@@ -54,5 +54,12 @@
         PostgreSqlRunQueriesAndCheckDatabase = 59,
         PostgreSqlRunSkeletonRunQueriesAndCheckDatabase = 60,
         PythonDjangoOrmExecutionStrategy = 61,
+        NodeJsV20PreprocessExecuteAndCheck = 67,
+        NodeJsV20PreprocessExecuteAndRunUnitTestsWithMocha = 68,
+        NodeJsV20PreprocessExecuteAndRunJsDomUnitTests = 69,
+        NodeJsV20PreprocessExecuteAndRunCodeAgainstUnitTestsWithMochaExecutionStrategy = 70,
+        NodeJsV20ZipPreprocessExecuteAndRunUnitTestsWithDomAndMocha = 71,
+        NodeJsV20ExecuteAndRunAsyncJsDomTestsWithReactExecutionStrategy = 72,
+        NodeJsV20ZipExecuteHtmlAndCssStrategy = 73,
     }
 }

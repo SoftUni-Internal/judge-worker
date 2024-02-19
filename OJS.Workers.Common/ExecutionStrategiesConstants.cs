334afa72683608ad7c4740ef21addc3649eb7362
Georgi Georgiev <gogo4ds@gmail.com>
2024-02-19 16:04:22 +0200

    updated java 17 to 21 version in constants (#183)

--- OJS.Workers.Common/ExecutionStrategiesConstants.cs
+++ OJS.Workers.Common/ExecutionStrategiesConstants.cs
@@ -33,11 +33,11 @@ namespace OJS.Workers.Common
             public const string JavaUnitTests = "java-unit-tests";
             public const string JavaZipFileCode = "java-zip-file-code";
             public const string JavaSpringAndHibernateProjectExecutionStrategy = "run-java-spring-data-junit-tests";
-            public const string Java17ProjectTests = "java-17-project-tests";
-            public const string Java17PreprocessCompileExecuteAndCheck = "java-17-code";
-            public const string Java17ZipFileCode = "java-17-zip-file-code";
-            public const string Java17UnitTests = "java-17-unit-tests";
-            public const string Java17SpringAndHibernateProjectExecutionStrategy = "run-java17-spring-data-junit-tests";
+            public const string Java21ProjectTests = "java-21-project-tests";
+            public const string Java21PreprocessCompileExecuteAndCheck = "java-21-code";
+            public const string Java21ZipFileCode = "java-21-zip-file-code";
+            public const string Java21UnitTests = "java-21-unit-tests";
+            public const string Java21SpringAndHibernateProjectExecutionStrategy = "run-java-21-spring-data-junit-tests";
 
             // JavaScript
             public const string JavaScriptCode = "javascript-code";
@@ -171,11 +171,11 @@ namespace OJS.Workers.Common
                     { ExecutionStrategyNames.JavaZipFileCode, ExecutionStrategyType.JavaZipFileCompileExecuteAndCheck },
                     { ExecutionStrategyNames.JavaUnitTests, ExecutionStrategyType.JavaUnitTestsExecutionStrategy },
                     { ExecutionStrategyNames.JavaSpringAndHibernateProjectExecutionStrategy, ExecutionStrategyType.JavaSpringAndHibernateProjectExecutionStrategy },
-                    { ExecutionStrategyNames.Java17PreprocessCompileExecuteAndCheck, ExecutionStrategyType.Java17PreprocessCompileExecuteAndCheck },
-                    { ExecutionStrategyNames.Java17ProjectTests, ExecutionStrategyType.Java17ProjectTestsExecutionStrategy },
-                    { ExecutionStrategyNames.Java17ZipFileCode, ExecutionStrategyType.Java17ZipFileCompileExecuteAndCheck },
-                    { ExecutionStrategyNames.Java17UnitTests, ExecutionStrategyType.Java17UnitTestsExecutionStrategy },
-                    { ExecutionStrategyNames.Java17SpringAndHibernateProjectExecutionStrategy, ExecutionStrategyType.Java17SpringAndHibernateProjectExecution },
+                    { ExecutionStrategyNames.Java21PreprocessCompileExecuteAndCheck, ExecutionStrategyType.Java21PreprocessCompileExecuteAndCheck },
+                    { ExecutionStrategyNames.Java21ProjectTests, ExecutionStrategyType.Java21ProjectTestsExecutionStrategy },
+                    { ExecutionStrategyNames.Java21ZipFileCode, ExecutionStrategyType.Java21ZipFileCompileExecuteAndCheck },
+                    { ExecutionStrategyNames.Java21UnitTests, ExecutionStrategyType.Java21UnitTestsExecutionStrategy },
+                    { ExecutionStrategyNames.Java21SpringAndHibernateProjectExecutionStrategy, ExecutionStrategyType.Java21SpringAndHibernateProjectExecution },
 
                     // Plain text
                     { ExecutionStrategyNames.PlainText, ExecutionStrategyType.CheckOnly },

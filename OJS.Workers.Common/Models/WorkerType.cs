46960277382f048dfdaa3a0b7c31af3789709133
Georgi Georgiev <gogo4ds@gmail.com>
2024-03-12 10:14:12 +0200

    Added new worker type for cloud legacy workers (#181)
    * Added new worker type for cloud legacy workers
    
    * Fixed bug
    
    * Fixed bug with Default worker type

--- OJS.Workers.Common/Models/WorkerType.cs
+++ OJS.Workers.Common/Models/WorkerType.cs
@@ -1,10 +1,15 @@
 ﻿namespace OJS.Workers.Common.Models
 {
+    /// <summary>
+    /// Subset of workers on which a submission is to be executed.
+    /// Stored in database, so be careful with changing values.
+    /// </summary>
     public enum WorkerType
     {
         Default = 0,
         Legacy = 1,
         Local = 2,
         Alpha = 3,
+        LegacyCloud = 4,
     }
 }
--- OJS.Workers/LocalWorkerServiceBase.cs
+++ OJS.Workers/LocalWorkerServiceBase.cs
@@ -88,11 +88,13 @@ namespace OJS.Workers
             var localWorkerSubmissionsForProcessing = new ConcurrentQueue<TSubmission>();
             var legacyWorkersSubmissionsForProcessing = new ConcurrentQueue<TSubmission>();
             var alphaWorkersSubmissionsForProcessing = new ConcurrentQueue<TSubmission>();
+            var legacyCloudWorkersSubmissionsForProcessing = new ConcurrentQueue<TSubmission>();
             var sharedLockObject = new object();
 
             var legacyWorkerTypesList = new List<WorkerType> { WorkerType.Legacy };
             var alphaWorkerTypesList = new List<WorkerType> { WorkerType.Alpha };
             var localWorkerTypesList = new List<WorkerType> { WorkerType.Local };
+            var legacyCloudWorkerTypesList = new List<WorkerType> { WorkerType.LegacyCloud };
 
            var isEnumParsed = Enum.TryParse(Settings.DefaultWorkerType, false, out WorkerType defaultWorkerType);
            if (!isEnumParsed)
@@ -111,6 +113,9 @@ namespace OJS.Workers
                     break;
                 case WorkerType.Local:
                     localWorkerTypesList.Add(WorkerType.Default);
+                    break;
+                case WorkerType.LegacyCloud:
+                    legacyCloudWorkerTypesList.Add(WorkerType.Default);
                     break;
                 default:
                     this.Logger.Fatal("Wrong default worker type");
@@ -133,7 +138,8 @@ namespace OJS.Workers
             workerThreads.AddRange(this.GetRemoteWorkers(
                 Settings.RemoteWorkerEndpoints,
                 legacyWorkersSubmissionsForProcessing,
-                sharedLockObject, formatterServiceFactory,
+                sharedLockObject,
+                formatterServiceFactory,
                 remoteSubmissionsFilteringService,
                 legacyWorkerTypesList));
             
@@ -144,6 +150,14 @@ namespace OJS.Workers
                 formatterServiceFactory,
                 remoteSubmissionsFilteringService,
                 alphaWorkerTypesList));
+
+            workerThreads.AddRange(this.GetRemoteWorkers(
+                Settings.LegacyCloudWorkerEndpoints,
+                legacyCloudWorkersSubmissionsForProcessing,
+                sharedLockObject,
+                formatterServiceFactory,
+                remoteSubmissionsFilteringService,
+                legacyCloudWorkerTypesList));
 
             workerThreads
                 .ToList()
